762e820a4399c05cd38cc9794b71759ed891620e
kratmy <kratmymail@gmail.com>
2026-02-19 22:19:05 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -9,4 +9,13 @@ local actualUrls = {
 
 
 _G.LunarisLoader = actualUrls
-loadstring(game:HttpGet(baseUrl .. actualUrls.main))()
+
+local success, result = pcall(function()
+    return game:HttpGet(baseUrl .. actualUrls.main)
+end)
+
+if success then
+    loadstring(result)()
+else
+    warn("lunaris: Ошибка загрузки основного файла. Проверьте интернет или GitHub.")
+end
