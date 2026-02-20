df900b1a4f83a2ff9c68ec0e84ad3c05ddbe5c98
kratmy <kratmymail@gmail.com>
2026-02-20 15:49:09 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,8 @@
 -- [[ lunaris Loader ]]
-local baseUrl = "https://raw.githubusercontent.com/kratmy/gti_te1/main/"
+--local baseUrl = "https://raw.githubusercontent.com/kratmy/gti_te1/main/"
 
 local actualUrls = {
+    baseUrl = "https://raw.githubusercontent.com/kratmy/gti_te1/main/",
     main   = "lunarisV9.lua",
     aim    = "aim2.lua",
     esp    = "esp2.lua"
