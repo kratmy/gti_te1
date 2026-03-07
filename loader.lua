0b7960073d78841c7e02feab485baf924fedc284
kratmy <kratmymail@gmail.com>
2026-03-08 01:56:53 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,20 +2,20 @@
 local baseUrl = "https://raw.githubusercontent.com/kratmy/gti_te1/main/"
 
 local actualUrls = {
-    main   = "lunarisV9.lua",
-    aim    = "aim2.lua",
-    esp    = "esp2.lua"
+	main   = "lunarisV9.lua",
+	aim    = "aim2.lua",
+	esp    = "esp2.lua"
 }
 
 
 _G.LunarisLoader = actualUrls
 
 local success, result = pcall(function()
-    return game:HttpGet(baseUrl .. actualUrls.main)
+	return game:HttpGet(baseUrl .. actualUrls.main)
 end)
 
 if success then
-    loadstring(result)()
+	loadstring(result)()
 else
-    warn("lunaris: Ошибка загрузки основного файла. Проверьте интернет или GitHub.")
+	warn("lunaris: Ошибка загрузки основного файла. Проверьте интернет или GitHub.")
 end
