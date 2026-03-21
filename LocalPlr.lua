4c0fbc540f632c6948bd3d1c247513da61cdbbb5
kratmy <kratmymail@gmail.com>
2026-03-21 22:07:34 +0800

    101stab

--- LocalPlr.lua
+++ LocalPlr.lua
@@ -4,6 +4,22 @@ local Initialized = false
 local GameWS = 16
 local GameJP = 50
 
+local mt = getrawmetatable(game)
+local oldNewIndex = mt.__newindex
+setreadonly(mt, false)
+
+mt.__newindex = newcclosure(function(t, k, v)
+    if not checkcaller() and t:IsA("Humanoid") then
+        if k == "WalkSpeed" and _G.Toggles and _G.Toggles.EnableWS and _G.Toggles.EnableWS.Value then
+            return -- Игра пытается поставить свою скорость? Игнорируем.
+        elseif k == "JumpPower" and _G.Toggles and _G.Toggles.EnableJP and _G.Toggles.EnableJP.Value then
+            return -- Игра пытается поставить свой прыжок? Игнорируем.
+        end
+    end
+    return oldNewIndex(t, k, v)
+end)
+setreadonly(mt, true)
+
 function LocalPlrModule.Run(Options, Toggles, LP)
 	local Char = LP.Character
 	local Hum = Char and Char:FindFirstChildOfClass("Humanoid")
